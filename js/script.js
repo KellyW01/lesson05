f15f4a56c8598eb526e23a78f46113b4bc901afb
kelly.w <kelly.conant.winn@gmail.com>
2023-05-22 11:54:44 -0400

    Added js code to make the image appear, disppear and reappear along with button color changes on branch cat-disappear

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 var button = document.querySelector(".show-cat");
-
 var cat = document.querySelector(".cat");
 
 button.addEventListener("click", function () {
-  cat.classList.add("show");
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait! Come back!";
+    button.classList.add("disappear");
+    // console.log("yes");
+  } else {
+    // console.log("no");
+    cat.classList.add("show");
+    button.innerText = "Just kidding, go away";
+    button.classList.remove("disappear");
+  }
 });
